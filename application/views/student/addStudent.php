d5558d52f4a03181a2c7f2a10c64e7906ce1de7d
jashimgit <jashim.ncn@gmail.com>
2019-12-29 01:01:10 +0600

    student html final

--- application/views/student/addStudent.php
+++ application/views/student/addStudent.php
@@ -2,30 +2,36 @@
 <hr />
 
 <div class="panel-body" style="width:600px;">
-    <form>
+    <form action="student/store" method="POST">
         <div class="form-group">
             <label>Student Name</label>
-            <input type="text" class="form-control span12">
+            <input type="text" class="form-control span12" name="s_name">
         </div>
         <div class="form-group">
             <label>Department</label>
-            <input type="text" class="form-control span12">
+            <select name="dep_namee" id="dep_name" class="form-control span12">
+                <option value="">1</option>
+                <option value="">1</option>
+                <option value="">1</option>
+                <option value="">1</option>
+                <option value="">1</option>
+            </select>
         </div>
         <div class="form-group">
             <label>Roll No.</label>
-            <input type="text" class="form-control span12">
+            <input type="text" class="form-control span12" name="s_roll">
         </div>
         <div class="form-group">
             <label>Reg. No.</label>
-            <input type="text" class="form-control span12">
+            <input type="text" class="form-control span12" name="s_reg">
         </div>
         <div class="form-group">
             <label>Session</label>
-            <input type="text" class="form-control span12">
+            <input type="text" class="form-control span12" name="s_session">
         </div>
         <div class="form-group">
             <label>Batch</label>
-            <input type="password" class="form-control span12">
+            <input type="password" class="form-control span12" name="s_batch">
         </div>
         <div class="form-group">
             <input type="submit" class="btn btn-primary" value="Submit">
